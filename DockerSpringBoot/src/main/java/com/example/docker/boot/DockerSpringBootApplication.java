cdd143dfd8fb87333989292496a02cc28e58cc22
KawitJain01 <kawitjain23@gmail.com>
2021-04-16 11:34:23 +0530

    Add files via upload

--- DockerSpringBoot/src/main/java/com/example/docker/boot/DockerSpringBootApplication.java
+++ DockerSpringBoot/src/main/java/com/example/docker/boot/DockerSpringBootApplication.java
@@ -2,8 +2,12 @@ package com.example.docker.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.controller.MyController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = MyController.class)
 public class DockerSpringBootApplication {
 
 	public static void main(String[] args) {
